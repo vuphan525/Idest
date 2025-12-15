ba634b49b26113298874d5fc11a61d518d4a2aca
LuckiPhoenix <huynhchihen2005@gmail.com>
2025-12-15 13:15:37 +0700

    perf(landing): add Head component to preload video asset for improved performance

---
+++ app/head.tsx
@@ -0,0 +1,12 @@
+export default function Head() {
+  return (
+    <>
+      <link
+        rel="preload"
+        href="/assets/cat-peeps.webm"
+        as="video"
+        type="video/webm"
+      />
+    </>
+  );
+}
